142fa509c61f62e4e55aee5d97a39bbea80af69a
Julien Kessels <julien.kessels@gmail.com>
2018-10-08 11:12:00 +0200

    added script to stop and remove all containers & images

--- linux_install.sh
+++ linux_install.sh
@@ -7,3 +7,4 @@ apt-cache policy docker-ce
 sudo apt-get install -y docker-ce
 sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod +x kill_all.sh
